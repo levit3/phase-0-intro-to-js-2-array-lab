ae58091bf76fe5f886054e741587a4bb2d2762ba
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-03-25 11:46:29 +0300

    creates functions with various array methods

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
+  return cats.push("Ralph");
+}
+function destructivelyPrependCat() {
+  return cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+  return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+  return cats.shift();
+}
+function appendCat() {
+  const newCats = [...cats, "Broom"];
+  return newCats;
+}
+function prependCat() {
+  const newCats = ["Arnold", ...cats];
+  return newCats;
+}
+function removeLastCat() {
+  const newCats = cats.slice(0, 2);
+  return newCats;
+}
+function removeFirstCat() {
+  const newCats = cats.slice(1);
+  return newCats;
+}
